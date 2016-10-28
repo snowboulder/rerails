546e6666c808341a2dfd7d39391625d4e6a7be4d
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-10-27 22:54:37 -0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to Refresher Rails!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:success] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
